da7a6059cf2fb36f9492972acd0549a016cba102
delia-pixel <tpatriciareinedia@gmail.com>
2023-11-18 10:00:22 +0100

    modal cpnt

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
 
-const Modal = ({open,onClose ,children}) => {
+const Modal = ({ open, onClose, children }) => {
   return (
-      <div onClick={onClose} className={`fixed inset-0 flex justify-center items-center transition-colors 
-    ${open? 'visible bg-black/20': 'invisible'}
-    `}>
-      {children}
+    <div
+      onClick={onClose}
+      className={`fixed inset-0 flex justify-center items-center transition-colors 
+    ${open ? "visible bg-black/20" : "invisible"}
+    `}
+    >
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        className={`bg-white rounded-md shadow p-6  transition-all ${
+          open ? `scale-100 opacity-100` : `scale-125 opacity-0`
+        }`}
+      >
+        {/* <button className="absolute  top-2 right-2  p-1 rounded-full text-gray-400 bg-white hover:bg-gray-50 hover:text-gray-600">
+          X
+        </button> */}
+        {children}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -2,49 +2,60 @@ import { star } from "../assets/icons";
 import { motion } from "framer-motion";
 import Modal from "./Modal";
 import { useState } from "react";
-import { view } from "../assets/icons";
+import view from "../assets/icons/view.png";
 
 const PopularProductCard = ({ imgURL, name, price }) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{
-        duration: 0.5,
-      }}
-      className="flex flex-1 flex-col w-full max-sm:w-full"
-    >
-      <motion.img
-        src={imgURL}
-        whileHover={{ scale: 1.1 }}
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
         transition={{
-          type: "spring",
-          stiffness: 260,
-          damping: 20,
+          duration: 0.5,
         }}
-        alt={name}
-        className="w-[280px] h-[280px]"
-      />
-      <div className="mt-8 flex justify-start gap-2.5 ">
-        <img src={star} alt="rating" width={24} height={24} />
-        <p className="font-monsterrat text-xl leading-normal text-slate-gray">
-          (4.5)
+        className="flex flex-1 flex-col w-full max-sm:w-full"
+      >
+        <motion.img
+          src={imgURL}
+          whileHover={{ scale: 1.1 }}
+          transition={{
+            type: "spring",
+            stiffness: 260,
+            damping: 20,
+          }}
+          alt={name}
+          className="w-[280px] h-[280px]"
+        />
+        <div className="mt-8 flex justify-start gap-2.5 ">
+          <img src={star} alt="rating" width={24} height={24} />
+          <p className="font-monsterrat text-xl leading-normal text-slate-gray">
+            (4.5)
+          </p>
+        </div>
+
+        <h3 className="mt-2 text-2xl font-palanquin font-semibold leading-normal">
+          {name}
+        </h3>
+
+        <p className="mt-2 font-semibold font-montserrat text-2xl leading-normal text-coral-red">
+          {price}{" "}
+          <img
+            className="cursor-pointer"
+            src={view}
+            alt="eye icon"
+            width={30}
+            height={30}
+            onClick={() => setOpen(true)}
+          />
         </p>
-      </div>
-
-      <h3 className="mt-2 text-2xl font-palanquin font-semibold leading-normal">
-        {name}
-      </h3>
-
-      <p className="mt-2 font-semibold font-montserrat text-2xl leading-normal text-coral-red">
-        {price} <img src={view} alt="eye icon" width={48} height={48} />
-      </p>
-    </motion.div>
+      </motion.div>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <h1>Hello here ! {name} </h1>
+      </Modal>
+    </>
   );
 };
-<Modal open={open} onClose={() => setOpen(false)}>
-  <h1>Hello here !</h1>
-</Modal>;
+
 export default PopularProductCard;
